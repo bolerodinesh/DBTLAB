b04a1c63774627e8c970ec56fbe295b945c3b6fc
Dinesh Kumar Chandra Sekar <bolerodineshaus@gmail.com>
2026-07-30 12:48:57 +0000

    Refactored sql

--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -1,67 +1,105 @@
 with
 
-    -- Import CTEs
-    customers as (select * from {{ source("jaffle_shop", "customers") }}),
+-- Import CTEs
 
-    orders as (select * from {{ source("jaffle_shop", "orders") }}),
+customers as (
 
-    payments as (select * from {{ source("stripe", "payment") }}),
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
-    -- Logical CTEs
-    payment_success as (
-        select
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1
-    ),
+),
 
-    paid_orders as (
-        select
-            orders.id as order_id,
-            orders.user_id as customer_id,
-            orders.order_date as order_placed_at,
-            orders.status as order_status,
-            payment_success.total_amount_paid,
-            payment_success.payment_finalized_date,
-            customers.first_name as customer_first_name,
-            customers.last_name as customer_last_name
-        from orders
-        left join payment_success on orders.id = payment_success.order_id
-        left join customers on orders.user_id = customers.id
-    ),
+orders as (
 
-    -- Final CTE
-    final as (
-        select
-            p.*,
-            row_number() over (order by p.order_id) as transaction_seq,
-            row_number() over (
-                partition by customer_id order by p.order_id
-            ) as customer_sales_seq,
-            case
-                when
-                    (
-                        rank() over (
-                            partition by customer_id
-                            order by order_placed_at, p.order_id
-                        )
-                        = 1
-                    )
-                then 'new'
-                else 'return'
-            end as nvsr,
-            sum(total_amount_paid) over (
-                partition by customer_id order by order_placed_at, p.order_id
-            ) as customer_lifetime_value,
-            first_value(p.order_placed_at) over (
-                partition by customer_id order by order_placed_at, p.order_id asc
-            ) as fdos
-        from paid_orders p
-        order by order_id
-    )
--- Simple Select Statmen
-select *
-from final
+  select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+payments as (
+
+  select * from {{ ref('stg_stripe__payment') }}
+
+),
+
+-- Logical CTEs
+
+completed_payments as (
+
+  select 
+    order_id,
+    max(payment_created) as payment_finalized_date,
+    sum(payment_amount) as total_amount_paid
+  from payments
+  where payment_status <> 'fail'
+  group by 1
+
+),
+
+paid_orders as (
+
+  select 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    orders.order_status,
+
+    completed_payments.total_amount_paid,
+    completed_payments.payment_finalized_date,
+
+    customers.first_name,
+    customers.last_name
+  from orders
+  left join completed_payments on orders.order_id = completed_payments.order_id
+  left join customers on orders.customer_id = customers.customer_id
+
+),
+
+-- Final CTE
+
+final as (
+
+  select
+    order_id,
+    customer_id,
+    order_date,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    first_name,
+    last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_date, order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_date
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(order_date) over (
+      partition by customer_id
+      order by order_date
+      ) as fdos
+
+    from paid_orders
+        
+)
+
+-- Simple Select Statement
+
+select * from final
+order by order_id
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -6,17 +6,22 @@ source as (
 
 ),
 
-renamed as (
+transformed as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status as order_status,
-        _etl_loaded_at
+  select
 
-    from source
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status as order_status,
+
+    case 
+        when status not in ('returned','return_pending') 
+        then order_date 
+    end as valid_order_date
+
+  from source
 
 )
 
-select * from renamed
+select * from transformed
