4835eae3971a554321fe619f489ecd9689fa07d8
Dinesh Kumar Chandra Sekar <bolerodineshaus@gmail.com>
2026-08-17 13:04:36 +0000

    jinja

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,4 +1,4 @@
-{% macro grant_select(schema=target.schema, role=transformer) %}
+{% macro grant_select(schema=target.schema, role=target.role) %}
         {% set sql %}
             grant usage on schema {{ schema }} to {{ role }} ;
             grant select on all tables in schema {{ schema }} to role {{ role }};
